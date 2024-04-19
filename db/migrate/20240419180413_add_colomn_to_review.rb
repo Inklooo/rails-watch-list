4a14f0825c8c76f3a8a75647ca990ea75e1012c0
Loic Jeannin <inklo.ca@gmail.com>
2024-04-19 14:05:20 -0400

    watch list

---
+++ db/migrate/20240419180413_add_colomn_to_review.rb
@@ -0,0 +1,5 @@
+class AddColomnToReview < ActiveRecord::Migration[7.1]
+  def change
+    add_column :reviews, :list_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_17_041407) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_19_180413) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,6 +37,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_17_041407) do
     t.datetime "updated_at", null: false
     t.integer "rating"
     t.string "poster_url"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "list_id"
   end
 
   add_foreign_key "bookmarks", "lists"
